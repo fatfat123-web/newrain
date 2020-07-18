843fba8ce430e53acbebc469ea9c6aee82ca5b02
Your NYour <653694078@qq.com>
2020-07-18 12:18:56 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  padding: 0;
+  margin: 0;
+}
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="gameScreen">
+        <audio id="audioCountDown" src='../assets/audio/countdown.mp3'></audio>
+        <audio id="audioOpen" src='../assets/audio/open-redpacket.mp3'></audio>
+    </div>
+</template>
+
+<script>
+    import main from '../js/main.js'
+    import p2 from '../js/p2.js'
+    import phaserSplit from '../js/phaser-split.js'
+    import pixi from '../js/pixi.js'
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+.body{
+    padding: 0;
+    margin: 0;
+}
+</style>
